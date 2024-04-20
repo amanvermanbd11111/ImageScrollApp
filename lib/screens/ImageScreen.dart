b36980b71cf7717b87744198754399402002698e
Aman Verma <amanvermanbd11111@gmail.com>
2024-04-20 15:43:12 +0530

    final

--- lib/screens/ImageScreen.dart
+++ lib/screens/ImageScreen.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:imagescroll/Controller/ImageController.dart';
@@ -31,6 +32,130 @@ class _MyHomePageState extends State<ImageScreen> {
           ),
           body:
 
+              kIsWeb?      RefreshIndicator(
+                  onRefresh: _handleRefresh,
+                  color: Colors.white,
+                  backgroundColor: Colors.blue,
+                  child:
+
+                  GetBuilder<ImageController>(
+                    init: ImageController(),
+                    builder: (s) => s.isLoading.value
+                        ? Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: widgetHeight * .06),
+                      child: const Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(
+                              color: Colors.purple,
+                            ),
+                          )),
+                    )
+                        :
+
+                    GridView.builder(
+                        scrollDirection: Axis.vertical,
+                        gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+
+                            crossAxisCount: calculateColumnCount(MediaQuery.of(context).size.width),
+
+
+                            crossAxisSpacing: 5.0,
+                            mainAxisSpacing: 5),
+                        itemCount: s.imageScrollResponse.value.hits?.length ?? 0,
+                        itemBuilder: (BuildContext ctx, index) {
+                          return InkWell(
+                            onTap: () {
+                              Get.to(FullScreenImage(
+                                  imageUrl: s.imageScrollResponse.value.hits![index].userImageURL.toString(),
+                                  tag: s.imageScrollResponse.value.hits?[index].user.toString()??""
+                              ));
+                              print("myindex" + index.toString());
+                            },
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                    padding: EdgeInsets.all(widgetHeight*.01),
+                                    //height: widgetHeight*.15,
+                                    alignment: Alignment.center,
+                                    //color: Colors.grey,
+                                    child:
+                                    Hero(
+                                      tag: s.imageScrollResponse.value.hits?[index].user.toString()??"",
+                                      child: CachedNetworkImage(
+                                        height: widgetHeight*.1,
+                                        imageUrl:s.imageScrollResponse.value.hits![index].userImageURL.toString(),
+                                        placeholder:
+                                            (context,
+                                            url) =>
+                                            Container(
+                                              margin: EdgeInsets.all(
+                                                  widgetHeight *
+                                                      .08),
+                                              height:
+                                              widgetHeight *
+                                                  .08,
+                                              width:
+                                              widgetHeight *
+                                                  .07,
+                                              child:
+                                              CircularProgressIndicator(
+                                                strokeWidth:
+                                                5,
+                                              ),
+                                            ),
+                                        errorWidget: (context,
+                                            url,
+                                            error) =>
+                                        new Icon(Icons
+                                            .error),
+                                      ),
+                                    )
+
+
+
+                                  // Image.asset(s.imageScrollResponse.value.hits![index].userImageURL.toString()),
+                                  //Image.network(s.imageScrollResponse.value.hits![index].userImageURL.toString()),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Container(
+                                        //height: widgetHeight*.02,
+                                        child: Row(
+                                            children:[
+                                              Image.asset("assets/icons/heart.png",height: widgetHeight*.02,),
+                                              Text(s.imageScrollResponse.value.hits?[index].likes.toString()??"",
+                                                style: TextStyle(fontSize: textWidth*.02),)
+                                            ]
+
+                                        ),
+                                      ),
+                                      Container(
+                                        //height: widgetHeight*.02,
+                                        child: Row(
+                                            children:[
+                                              Image.asset("assets/icons/view.png",height: widgetHeight*.02),
+                                              Text(s.imageScrollResponse.value.hits?[index].views.toString()??"",
+                                                style: TextStyle(fontSize: textWidth*.02),)
+                                            ]
+
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                    ),
+                  ))
+              :
     RefreshIndicator(
     onRefresh: _handleRefresh,
     color: Colors.white,
@@ -150,9 +275,42 @@ class _MyHomePageState extends State<ImageScreen> {
                       );
                     }
                     ),
-          )),)
+          )),
+
+
+
+
+      )
     );
   }
+
+
+
+
+
+  int calculateColumnCount(double screenWidth) {
+    // Adjust these values based on your needs
+    const double minWidth = 200.0;
+    const int maxColumns = 5;
+
+    int columnCount = (screenWidth / minWidth).floor();
+
+
+    if (columnCount<=0)
+  {
+    columnCount++;
+    print("coulums"+columnCount.toString());
+
+
+
+
+  }
+    print("countss"+columnCount.toString());
+
+
+    return columnCount > maxColumns ? maxColumns :     columnCount;
+  }
+
   Future<void> _handleRefresh() async {
     // Simulate network fetch or database query
     await Future.delayed(Duration(seconds: 2));
--- lib/screens/fullImageScreen.dart
+++ lib/screens/fullImageScreen.dart
@@ -15,45 +15,59 @@ class FullScreenImage extends StatefulWidget {
 class _FullScreenImageState extends State<FullScreenImage> {
   @override
   Widget build(BuildContext context) {
+
     double widgetHeight = MediaQuery.of(context).size.height;
     double textWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black87,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: (){
-                Get.back();
-              },
-              child: Container(
-              margin: EdgeInsets.only(top: widgetHeight*.03,
-              bottom: widgetHeight*.04,
-                  right: textWidth*.02),
-                alignment: Alignment.topRight,
-                child:Icon(Icons.close,size: widgetHeight*.04,
-                color: Colors.white,) ,
-              ),
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.only(top: widgetHeight*.05),
-              child: Center(
-                child: Hero(
-                  tag: widget.tag,
-                  child: CachedNetworkImage(
-                    width: MediaQuery.of(context).size.width,
-                    //height: widgetHeight,
-                    fit: BoxFit.contain,
-                    imageUrl: widget.imageUrl,
-                  ),
+        body:
+        
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Get.back();
+                },
+                child: Container(
+                margin: EdgeInsets.only(
+                    //top: widgetHeight*.03,
+                //bottom: widgetHeight*.04,
+                    right: textWidth*.02),
+                  alignment: Alignment.topRight,
+                  child:Icon(Icons.close,
+                    size: widgetHeight*.04,
+                  color: Colors.white,) ,
                 ),
               ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+              //  height: widgetHeight,
+               height: (widgetHeight - widgetHeight*.03),
+                alignment: Alignment.topLeft,
 
-            ),
-          ],
+                //margin: EdgeInsets.only(top: widgetHeight*.05),
+                child: Center(
+                  child: Hero(
+                    tag: widget.tag,
+                    child: Expanded(
+                      child: Image.network(
+                        width:(widgetHeight - widgetHeight*.03),
+                        // width: MediaQuery.of(context).size.width,
+                        //height: widgetHeight,
+                        fit: BoxFit.contain,
+                        widget.imageUrl,
+                      ),
+                    ),
+                  ),
+                ),
+
+              ),
+            ],
+          ),
         ),
       ),
     );
